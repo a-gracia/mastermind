3577d37c5bdef00360bf9aa2d5b0978613951325
Andres <18741308+a-gracia@users.noreply.github.com>
2025-11-09 02:26:38 -0500

    feat: add maxmin algorithm for CPU calculation

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 class Mastermind
   attr_reader :user_guesses, :secret_code
 
@@ -10,52 +12,85 @@ class Mastermind
     player = 0
 
     until [1, 2].include?(player)
-      puts 'Choose your game mode'
-      puts '1. Guess mode'
-      puts '2. Secret code creator mode'
+      puts "Choose your game mode\n1. Guess mode\n2. Secret code creator mode"
       player = gets.chomp.to_i
     end
 
     if player == 1
       puts "\n\n*** Guess mode ***\n\n"
-      @secret_code = ask_cpu_guess
+      @secret_code = SECRET_CODE_SLOTS.times.map { |_i| COLORS.sample }
       @player = :user
     else
       puts "*** Secret code creator mode ***\n\n"
       @secret_code = ask_user_guess
       @player = :cpu
+
+      @possible_codes = []
+      COLORS.repeated_permutation(4) { |permutation| @possible_codes << permutation }
+      @reduced_list = @possible_codes
     end
 
-    @user_guesses = []
-    @feedback = []
+    @current_hint = []
+    @attempt = 0
   end
 
   def ask_cpu_guess
-    SECRET_CODE_SLOTS.times.map { |_i| COLORS.sample }
+    if @attempt == 1
+      %i[red red blue blue]
+    else
+      @reduced_list = @reduced_list.select { |code| check_hints(@guess, code) == @current_hint }
+
+      return @reduced_list.first if @reduced_list.length == 1
+
+      code_hint_count = {}
+
+      @possible_codes.each do |code|
+        code_hint_count[code.join('.')] = @reduced_list.each_with_object(Hash.new(0)) do |reduced_code, result|
+          result[check_hints(code, reduced_code).join] += 1
+          result
+        end
+      end
+
+      code_hint_count2 = {}
+
+      code_hint_count.each do |key, val|
+        mapped_val = val.map { |k, v| v }
+        code_hint_count2[key] = mapped_val.sum - mapped_val.max
+      end
+
+      # Return value with max score
+      code_hint_count2.max_by { |key, value| value }[0].split('.').map { |color| color.to_sym }
+    end
   end
 
-  def play
+  def check_hints(secret_code, user_guess)
     temp_secret_code = []
     guess = []
 
     # If there is a match in the guess, store a red hint
     # and delete the element in both arrays
     hints = []
-    @secret_code.each_with_index do |secret_color, i|
-      if secret_color == @user_guesses.last[i]
+    secret_code.each_with_index do |secret_color, i|
+      if secret_color == user_guess[i]
         hints << :red
       else
         temp_secret_code << secret_color
-        guess << @user_guesses.last[i]
+        guess << user_guess[i]
       end
     end
 
     # Then check if the remaining guesses are included in
     # the secret code
-    temp_secret_code.each { |secret_color| hints << :white if guess.include? secret_color }
+
+    temp_secret_code.each do |secret_color|
+      next unless guess.include? secret_color
+
+      hints << :white
+      guess.delete_at(guess.index(secret_color))
+    end
 
     # return shuffled hints
-    hints.shuffle
+    hints
   end
 
   def ask_user_guess
@@ -83,18 +118,18 @@ class Mastermind
 
   def give_feedback
     puts 'Your guess is:'
-    puts @user_guesses.last.map { |color| color.to_s.colorize(color) }.join(' ')
+    puts @guess.map { |color| color.to_s.colorize(color) }.join(' ')
     puts 'These are your hints:'
-    puts play.map { |color| color.to_s.colorize(color) }.join(' ')
+    puts @current_hint.shuffle.map { |color| color.to_s.colorize(color) }.join(' ')
   end
 
   def give_results
     # Give win/lose message
-    puts play.count(:red) == SECRET_CODE_SLOTS ? "You won!\n\n" : "You lost!\n\n"
+    puts @current_hint.count(:red) == SECRET_CODE_SLOTS ? "You won!\n\n" : "You lost!\n\n"
 
     # Show last guess and secret code
     puts 'Your last guess was:'
-    puts @user_guesses.last.map { |color| color.to_s.colorize(color) }.join(' ')
+    puts @guess.map { |color| color.to_s.colorize(color) }.join(' ')
 
     puts 'The secret code is:'
     puts secret_code.map { |color| color.to_s.colorize(color) }.join(' ')
@@ -103,13 +138,15 @@ class Mastermind
   def run_guess_mode
     # Cycle until attempt limit is reached
     ATTEMPT_LIMIT.times do
+      @attempt += 1
       # Ask user guess
-      guess = @player == :user ? ask_user_guess : ask_cpu_guess
-      @user_guesses << guess
+      @guess = @player == :user ? ask_user_guess : ask_cpu_guess
+      # Check hints
+      @current_hint = check_hints(@secret_code, @guess)
       # Give feedback
       give_feedback
       # If guess is correct, end loop
-      break if play.count(:red) == SECRET_CODE_SLOTS
+      break if @current_hint.count(:red) == SECRET_CODE_SLOTS
     end
     # Give results
     give_results
--- main.rb
+++ main.rb
@@ -1,6 +1,13 @@
 require 'colorize'
 require_relative 'lib/mastermind'
 
+require 'pry-byebug'
+
+# check_hints(%i[red red blue blue], %i[red blue red green])
+# rww
+# p Mastermind.check_hints(%i[red blue red green], %i[blue red blue red])
+# www
+
 game = Mastermind.new
 
 game.run_guess_mode
